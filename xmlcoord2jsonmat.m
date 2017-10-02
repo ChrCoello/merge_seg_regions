d066d67a3b2c123706dabfdad970f5fc9484c1ea
Christopher Coello <christopher.coello@gmail.com>
2017-10-02 11:59:17 +0200

    Real world coordinates
    Also adds a functoin to transform xml to mat

--- xmlcoord2jsonmat.m
+++ xmlcoord2jsonmat.m
@@ -27,19 +27,19 @@ C=S.Children;
 data=C(~cellfun('isempty',strfind({C(:).Name},'slice')));
 
 for iR = 1:length(data)
-   json_struct.slice(iR).nr  = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'nr'))).Value;
-   json_struct.slice(iR).filename = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'filename'))).Value;
-   json_struct.slice(iR).height = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'height'))).Value;
-   json_struct.slice(iR).width  = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'width'))).Value;
-   llt=data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'anchoring'))).Value;
-   equal_sym=strfind(llt,'=');
-   comma_sym=[0 strfind(llt,',') length(llt)];
-   for iT=1:length(equal_sym)
-    json_struct.slice(iR).(llt(comma_sym(iT)+1:equal_sym(iT)-1))=str2double(llt((equal_sym(iT)+1):(comma_sym(iT+1)-1)));
-   end
-   json_struct.slice(iR).transf_mat = [json_struct.slice(iR).ux json_struct.slice(iR).ox json_struct.slice(iR).vx;...
-       json_struct.slice(iR).uy json_struct.slice(iR).oy json_struct.slice(iR).vy;...
-       json_struct.slice(iR).uz json_struct.slice(iR).oz json_struct.slice(iR).vz];
+    json_struct.slice(iR).nr  = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'nr'))).Value;
+    json_struct.slice(iR).filename = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'filename'))).Value;
+    json_struct.slice(iR).height = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'height'))).Value;
+    json_struct.slice(iR).width  = data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'width'))).Value;
+    llt=data(iR).Attributes(~cellfun('isempty',strfind({data(iR).Attributes(:).Name},'anchoring'))).Value;
+    equal_sym=strfind(llt,'=');
+    comma_sym=[0 strfind(llt,',') length(llt)];
+    for iT=1:length(equal_sym)
+        json_struct.slice(iR).(llt(comma_sym(iT)+1:equal_sym(iT)-1))=str2double(llt((equal_sym(iT)+1):(comma_sym(iT+1)-1)));
+    end
+    json_struct.slice(iR).transf_mat = [json_struct.slice(iR).ux json_struct.slice(iR).ox json_struct.slice(iR).vx;...
+        json_struct.slice(iR).uy json_struct.slice(iR).oy json_struct.slice(iR).vy;...
+        json_struct.slice(iR).uz json_struct.slice(iR).oz json_struct.slice(iR).vz];
 end
 json_fn = fullfile(pp,[tt '.json']);
 savejson('',json_struct,json_fn);
